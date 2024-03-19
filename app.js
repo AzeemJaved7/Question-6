919a074cc6e7e4b455419c4fdf7baf68311551d1
AzeemJaved7 <163923406+AzeemJaved7@users.noreply.github.com>
2024-03-19 20:25:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+let PersonName = "\n\t Azeem Javed \t\n";
+// console.log(PersonName);
+let without_whitespace = PersonName.trim();
+console.log([without_whitespace]);
+// let CountryName: string="Pakistan";
+// console.log(CountryName);
---
+++ app.ts
@@ -0,0 +1,11 @@
+let PersonName: string= "\n\t Azeem Javed \t\n";
+
+// console.log(PersonName);
+
+
+let without_whitespace: string= PersonName.trim();
+console.log([without_whitespace]);
+
+// let CountryName: string="Pakistan";
+// console.log(CountryName);
+
